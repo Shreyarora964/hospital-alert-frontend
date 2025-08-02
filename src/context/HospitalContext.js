793727f13c465aec85821f7b77a79323c67050dc
OmPethani <oapethani@myseneca.ca>
2025-08-02 03:31:07 -0400

    Update HospitalContext.js

--- src/context/HospitalContext.js
+++ src/context/HospitalContext.js
@@ -12,7 +12,7 @@ export const HospitalProvider = ({ children }) => {
   useEffect(() => {
   console.log("Fetching hospital data...");
   if (hospitals.length === 0) {
-    axios.get("https://4819525a48dc.ngrok-free.app/api/hospitals")
+    axios.get("https://bb131e85a12f.ngrok-free.app/api/hospitals")
       .then(res => {
         console.log("API Response:", res.data); // ✅ ADD THIS
         if (res.data.status === "success") {
